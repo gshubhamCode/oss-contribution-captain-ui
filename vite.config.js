ded7977c8bbe2f3babdcc1aa89accf64997e838f
Shubham Gupta <shubham18p@gmail.com>
2025-06-29 14:50:01 +0530

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/oss-contribution-captain-ui/', // must match repo name
+  base: '/', 
   plugins: [react()],
 })
